5e69be4e958f1ba7b9c316b64c7cc430d57047e2
Vibhor Dubey <vibhor.dubey@hpe.com>
2023-03-06 17:35:13 +0530

    Python-Programs-Concepts|Programs|If...Elif

---
+++ 1.If...Elif/If...Elif-Function-Approach.py
@@ -0,0 +1,18 @@
+def findMaximumOfThree(a,b,c):
+    list = [a,b,c]
+    return max(list)
+
+# Driver code:
+# Talking user input
+x = int(input("Enter First Number:"))
+y = int(input("Enter Second Number:"))
+z = int(input("Enter Third Number:"))
+
+print("Maximum Number Is: ",findMaximumOfThree(x,y,z))
+
+#python3 If...Elif-Function-Approach.py
+#Enter First Number:12
+#Enter Second Number:42
+#Enter Third Number:9934
+#Maximum Number Is:  9934
+
