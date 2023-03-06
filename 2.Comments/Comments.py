eee64dd0b1ffb44ee4f11c9df2b3a9358e6eea65
Vibhor Dubey <vibhor.dubey@hpe.com>
2023-03-06 17:56:47 +0530

    Python-Programs-Concepts|Programs|Comments

---
+++ 2.Comments/Comments.py
@@ -0,0 +1,15 @@
+# Single line comment
+# Multi
+# Line
+# Comment
+
+def add(a,b):
+    """Functions to add the value of a and b"""
+    return a+b
+
+print(add.__doc__)
+print(add(2,3))
+
+#  python 2.Comments/Comments.py
+# Functions to add the value of a and b
+# 5
